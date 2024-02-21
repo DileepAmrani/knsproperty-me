a643ff2f6b2103319193c054403f6951229b1056
DileepAmrani <amranidileep44@gmail.com>
2024-02-21 20:30:16 +0500

    l

--- src/app/about-us/team/[slug]/page.tsx
+++ src/app/about-us/team/[slug]/page.tsx
@@ -1,9 +1,26 @@
 import Container from "@/blocks/atoms/container";
 import strapi from "@/utils/strapi";
 import Image from "next/image";
+import { GetServerSideProps } from "next";
 
-export default async function Page({ params: { slug } }: any) {
-  const { attributes } = await getData(slug);
+interface TeamMember {
+  Name: string;
+  Position: string;
+  Description: string;
+  Profile: {
+    data: {
+      attributes: {
+        url: string;
+      };
+    };
+  };
+}
+
+interface PageProps {
+  attributes: TeamMember;
+}
+
+export default function Page({ attributes }: PageProps) {
   return (
     <main>
       <Container>
@@ -16,6 +33,8 @@ export default async function Page({ params: { slug } }: any) {
                 src={attributes.Profile.data.attributes.url}
                 fill
                 alt={attributes.Name}
+                width={300} // Adjust as needed
+                height={300} // Adjust as needed
               />
             </div>
           </section>
@@ -39,22 +58,30 @@ export default async function Page({ params: { slug } }: any) {
   );
 }
 
-async function getData(slug: string) {
-  const blog = await strapi.find<any>("teams", {
-    populate: ["*", "Profile"],
-    filters: {
-      slug: slug,
-    },
-  });
-  return blog.data[0];
-}
+// export const getServerSideProps: GetServerSideProps<PageProps> = async ({
+//   params,
+// }) => {
+//   const slug = params?.slug;
+//   if (!slug || typeof slug !== 'string') {
+//     return {
+//       notFound: true,
+//     };
+//   }
+//   const attributes = await getData(slug);
+//   return {
+//     props: {
+//       attributes,
+//     },
+//   };
+// };
 
-export async function generateStaticParams() {
-  const slugs = await strapi.find<any>("teams", {
-    fields: ["id", "slug"],
-  });
-
-  return slugs.data.map((post: any) => ({
-    slug: post.attributes.slug,
-  }));
-}
+// async function getData(slug: string): Promise<TeamMember> {
+//   const teamMemberResponse = await strapi.find<TeamMember>("teams", {
+//     populate: ["Profile"],
+//     filters: {
+//       slug: slug,
+//     },
+//   });
+//   const teamMember = teamMemberResponse.data[0];
+//   return teamMember;
+// }
